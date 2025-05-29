131f41ac0d55045e2267456aa79ddc049e702b08
smartinm67@edu <132890727+smartinm67edu@users.noreply.github.com>
2025-05-29 13:10:00 +0200

    borrado

--- assets/backend/models/Usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const usuarioSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'user'], default: 'user' }
-});
-
-module.exports = mongoose.model('Usuario', usuarioSchema);
